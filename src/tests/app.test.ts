2d7d329f4294e8ab4c3afb69539a59f2efda00e4
Wasya1212 <wasya1212cool@gmail.com>
2022-10-09 22:53:15 +0300

    added pretier

---
+++ src/tests/app.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest'
+
+import app from '../index'
+
+it('GET / - should return hello world', (done) => {
+  request(app).get('/').expect('hello world').end(done)
+})
+
+it('POST / - should return hello world', (done) => {
+  request(app).post('/').expect({ result: 'hello world' }).end(done)
+})
